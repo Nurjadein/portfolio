216d2f623b643abc5c0b999d40d63e08accec66d
Nurjadein <nurjadeinabdulmorid@gmail.com>
2026-08-12 21:30:39 +0800

    fill empty files

--- src/components/AppNavbarComp.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    //comment
-</template>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="min-h-screen">
+    <AppNavbar />
+
+    <main>
+      <RouterView />
+    </main>
+
+    <AppFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppFooter from '@/components/AppFooter.vue'
+import AppNavbar from '@/components/AppNavbar.vue'
+</script>
