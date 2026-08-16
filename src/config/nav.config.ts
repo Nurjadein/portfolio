af8a3a51b381cf478f9ae8d294852c95035e164a
Nurjadein <nurjadeinabdulmorid@gmail.com>
2026-08-16 18:03:36 +0800

    nav config

---
+++ src/config/nav.config.ts
@@ -0,0 +1,24 @@
+import type { INavItems } from "@/interfaces/nav.interface";
+
+export const navItems: INavItems[] = [
+  {
+    label: 'About',
+    href: '#about',
+  },
+  {
+    label: 'Skills',
+    href: '#skills',
+  },
+  {
+    label: 'Experience',
+    href: '#experience',
+  },
+  {
+    label: 'Projects',
+    href: '#projects',
+  },
+  {
+    label: 'Contact',
+    href: '#contact',
+  },
+];
