c26a68057744522a029f8ee3c477cb6f0e4c055b
Nurjadein <nurjadeinabdulmorid@gmail.com>
2026-08-18 09:40:11 +0800

    update description

--- src/config/about.config.ts
+++ src/config/about.config.ts
@@ -6,9 +6,9 @@ export const aboutData:IAboutData = {
   description: `
         Software developer with nearly 3 years of experience building web applications using Vue, TypeScript, NestJS, and MySQL.
 
-        Graduated with a BS in Information Technology major in Database Systems from Mindanao State University Main Campus, Marawi City. Experienced in developing maintainable frontend and backend solutions, API integration, state management, and database-driven applications.
+        Graduated with a BS in Information Technology major in Database Systems from Mindanao State University Main Campus, Marawi City, Philippines. Experienced in developing maintainable frontend and backend solutions, API integration, state management, and database-driven applications.
 
-        I enjoy building scalable, production-ready systems using industry-standard practices, with a strong focus on clean architecture, maintainability, and long-term growth.
+        I am into building scalable, production-ready systems using industry-standard practices, with a strong focus on clean architecture, maintainability, and long-term growth.
         `,
 
   highlights: [
