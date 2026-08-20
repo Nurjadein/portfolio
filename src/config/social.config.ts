872b5bd8a5ebe3222373067d2803d3b1abf5768e
Nurjadein <nurjadeinabdulmorid@gmail.com>
2026-08-20 20:50:37 +0800

    shape the the social config data

--- src/config/social.config.ts
+++ src/config/social.config.ts
@@ -1,5 +1,16 @@
 export const socialLinks = {
-  github: '',
-  linkedin: '',
-  email: '',
-};
+  github: {
+    label: 'GitHub',
+    url: 'https://github.com/Nurjadein',
+  },
+
+  linkedin: {
+    label: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/nurjadein/',
+  },
+
+  email: {
+    label: 'Email',
+    url: 'mailto:nurjadeinabdulmorid@gmail.com',
+  },
+}
