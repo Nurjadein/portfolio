a9acdf1258f5f26b5098ae4b308f5aada7ca11e4
Nurjadein <nurjadeinabdulmorid@gmail.com>
2026-08-20 16:30:01 +0800

    adjust padding and edges

--- src/components/common/AppNavbar.vue
+++ src/components/common/AppNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="sticky top-0 z-70">
     <nav
-      class="mx-auto flex max-w-7xl items-center justify-between rounded-2xl border border-slate-800/70 bg-slate-900/40 px-6 py-4 backdrop-blur-sm"
+      class="mx-auto flex max-w-7xl items-center justify-between border border-slate-800/70 bg-slate-900/40 px-6 py-4 backdrop-blur-sm"
     >
       <a
         href="#"
