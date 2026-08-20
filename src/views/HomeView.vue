fbd60c89b2f07c8d187c509f58b7c0bf43a7fab9
Nurjadein <nurjadeinabdulmorid@gmail.com>
2026-08-20 16:33:21 +0800

    drop projects section beacause it is already in experience section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,6 @@
   <AboutSection />
   <SkillsSection />
   <ExperienceSection />
-  <ProjectsSection />
   <ContactSection />
 </template>
 
@@ -12,6 +11,5 @@ import HeroSection from '@/components/home/HeroSection.vue'
 import AboutSection from '@/components/home/AboutSection.vue'
 import SkillsSection from '@/components/home/SkillsSection.vue'
 import ExperienceSection from '@/components/home/ExperienceSection.vue'
-import ProjectsSection from '@/components/home/ProjectsSection.vue'
 import ContactSection from '@/components/home/ContactSection.vue'
 </script>
