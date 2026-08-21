4db9c8a1cd7293e8afae0cb7ecb004283555b9c4
Nurjadein <nurjadeinabdulmorid@gmail.com>
2026-08-21 21:29:39 +0800

    include icons to social links

--- src/config/social.config.ts
+++ src/config/social.config.ts
@@ -1,16 +1,21 @@
+import githubIcon from '@/assets/icons/github.svg'
+import linkedinIcon from '@/assets/icons/linkedIn.png'
+import gmailIcon from '@/assets/icons/gmail.png'
+
 export const socialLinks = {
   github: {
     label: 'GitHub',
     url: 'https://github.com/Nurjadein',
+    icon: githubIcon,
   },
-
   linkedin: {
     label: 'LinkedIn',
     url: 'https://www.linkedin.com/in/nurjadein/',
+    icon: linkedinIcon,
   },
-
   email: {
     label: 'Email',
     url: 'mailto:nurjadeinabdulmorid@gmail.com',
+    icon: gmailIcon,
   },
 }
