6bf5290813b3bbd84d157151c9254c11e68626ae
Nurjadein <nurjadeinabdulmorid@gmail.com>
2026-08-21 21:28:44 +0800

    social icon component

---
+++ src/components/common/SocialIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <span
+    class="
+      flex
+      h-8
+      w-8
+      shrink-0
+      items-center
+      justify-center
+      rounded-lg
+      border
+      border-slate-700
+      bg-slate-200
+    "
+  >
+    <img
+      :src="icon"
+      :alt="label"
+      class="h-4 w-4 object-contain"
+    />
+  </span>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  icon: string
+  label: string
+}>()
+</script>
