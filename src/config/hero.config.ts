c2d3a4626eb24046091503a9973dd73ca1b510a1
Nurjadein <nurjadeinabdulmorid@gmail.com>
2026-08-24 11:28:58 +0800

    role change

--- src/config/hero.config.ts
+++ src/config/hero.config.ts
@@ -3,7 +3,7 @@ import type { IHeroData } from '@/interfaces/hero.interface';
 export const heroData: IHeroData = {
   greeting: 'Hello, I am',
   name: 'Nurjadein Abdulmorid',
-  title: 'Frontend Developer',
+  title: 'Softwate Developer',
   description:
     'Building scalable and maintainable web applications with modern JavaScript and TypeScript technologies.',
   contactButtonText: 'Contact Me',
