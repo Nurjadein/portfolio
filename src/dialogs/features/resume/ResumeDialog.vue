fa38ae4b18e818b9e74ca49bd2f6c8c92c86be33
Nurjadein <nurjadeinabdulmorid@gmail.com>
2026-09-13 14:22:09 +0800

    view resume in dialog

---
+++ src/dialogs/features/resume/ResumeDialog.vue
@@ -0,0 +1,32 @@
+<template>
+  <BaseDialog
+    title="Nurjadein's Resume"
+    width="90vw"
+    height="90vh"
+    @close="handleClose"
+  >
+    <iframe
+      :src="resumeUrl"
+      title="Nurjadein's Resume"
+      class="h-full w-full border-0"
+    />
+  </BaseDialog>
+</template>
+
+<script setup lang="ts">
+import BaseDialog from '@/dialogs/core/BaseDialog.vue'
+import resumeUrl from '@/assets/my-cv.pdf'
+
+const props = defineProps<{
+  callback?: () => void
+}>()
+
+const emit = defineEmits<{
+  close: []
+}>()
+
+function handleClose() {
+  props.callback?.()
+  emit('close')
+}
+</script>
