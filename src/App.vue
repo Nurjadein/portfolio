59ecfaecce2d6bc8e6bb46a252ad8a0288246522
Nurjadein <nurjadeinabdulmorid@gmail.com>
2026-09-16 13:48:17 +0800

    put loading

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
 <template>
+  <AppLoader ref="loader" />
+
   <RouterView />
 </template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+import AppLoader from '@/components/common/AppLoader.vue'
+
+const loader = ref<InstanceType<typeof AppLoader> | null>(null)
+
+onMounted(() => {
+  setTimeout(() => {
+    loader.value?.hide()
+  }, 700)
+})
+</script>
